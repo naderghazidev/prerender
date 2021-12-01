0e421baf5e5a558333dd0efca63895e1b73be3fa
naderghazidev <43502458+naderghazidev@users.noreply.github.com>
2021-12-01 15:53:54 +0330

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var prerender = require('./lib');
 
 var server = prerender({
-  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage'],    forwardHeaders: true,    chromeLocation: '/usr/bin/chromium-browser'
+  chromeLocation:'/usr/bin/chromium-browser'
 });
 
 server.use(prerender.sendPrerenderHeader());
